e119473c7deddb807e514070b31e782a3cbf9c55
qianphong <qianphong@163.com>
2022-05-05 17:48:41 +0800

    chore: update

--- note/demo.ts
+++ note/demo.ts
@@ -22,3 +22,34 @@ type res2 = GetFist<['1', 2, 3]>
 
 type GetRefProps<Props> = Props extends { ref?: any } ? Props['ref'] : never
 type GetRefPropsRes = GetRefProps<{ ref?: undefined; a: 2 }>
+
+type A = never extends never ? true : false
+
+type J<T> = T extends string ? true : false
+
+type B = J<never>
+
+type MergeArr<A extends unknown[], B extends unknown[]> = {
+  [K in keyof A]: K extends keyof B ? [A[K], B[K]] : undefined
+}
+
+type Res3 = MergeArr<[1, 2], ['a', 'b']>
+const b: Res3 = [
+  [1, 'a'],
+  [2, 'b'],
+]
+
+type ToMutable<T> = {
+  -readonly [key in keyof T]: T[key]
+}
+type ToRequired<T> = {
+  [key in keyof T]-?: T[key]
+}
+
+type Test = {
+  readonly a: string
+  b?: number
+  c: boolean
+}
+type Test1 = ToMutable<Test>
+type Test2 = ToRequired<Test>
---
+++ note/深入浅出Node.md
@@ -0,0 +1,4 @@
+## Chrome 和 Node
+
+![chrome&node](../images/chrome%26node.png)
+除了 HTML、WebKit 和显卡这些 UI 相关的没有支持外，Node 的结构和 Chrome 非常相似，都是基于事件驱动的衣不架构，浏览器通过事件驱动来服务界面上的交互，Node 通过事件驱动来服务 I/O。
